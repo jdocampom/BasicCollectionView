0b970a8e37ea89350aae480083da577904e0dd3c
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2021-10-20 15:03:20 -0500

    Final Commit

---
+++ BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Juan Diego Ocampo on 10/20/21.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    
+}
